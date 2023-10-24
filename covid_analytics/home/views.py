431d73a23d1a1fe55db8d0f081e144ac3d70c7fd
Kumar Ankur <kr.ankur7@gmail.com>
2023-10-24 21:27:47 +0530

    Added Home page

--- covid_analytics/home/views.py
+++ covid_analytics/home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request,'index.html')
+def home(request):
+    background_image_url='images/covidbanner.jpg' 
+    return render(request,'home/home.html',{'background_image_url': background_image_url})
+
+
+def dashboard(request):
+    return render(request,'home/dashboard.html')
