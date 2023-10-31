3c36810580f7eb0675ca501fac5ad8f80ecc9a9d
Yash <sadhwaniyash6@gmail.com>
2023-10-31 22:50:04 +0530

    added dashboard with dummy data

--- covid_analytics/home/views.py
+++ covid_analytics/home/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+import matplotlib.pyplot as plt
+import io
+import urllib
+import base64
 
 # Create your views here.
 def home(request):
@@ -7,4 +11,28 @@ def home(request):
 
 
 def dashboard(request):
-    return render(request,'home/dashboard.html')
+    total_deaths = 15000
+    total_vaccinated = 100000
+
+    categories = ['Category A', 'Category B', 'Category C']
+    values = [20, 14, 23]
+
+    plt.figure(figsize=(8, 6))
+    plt.bar(categories, values)
+    plt.title('Bar Chart')
+    plt.xlabel('Categories')
+    plt.ylabel('Values')
+    
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plot_data = urllib.parse.quote(base64.b64encode(buffer.read()).decode())
+
+
+
+    context = {
+        'total_deaths': total_deaths,
+        'total_vaccinated': total_vaccinated,
+        'plot_data':plot_data
+    }
+    return render(request,'home/dashboard.html',context)
