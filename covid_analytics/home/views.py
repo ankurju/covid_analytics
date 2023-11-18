61df6c25842059ce50f99330bc42542232f7d715
Kumar Ankur <kr.ankur7@gmail.com>
2023-11-18 23:45:16 +0530

    Added different graphs for different case

--- covid_analytics/home/views.py
+++ covid_analytics/home/views.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import io
 import urllib
 import base64
+from home.test.test import process_data
 
 # Create your views here.
 def home(request):
@@ -12,63 +13,51 @@ def home(request):
 
 def dashboard(request):
 
-    categories = ['Category A', 'Category B', 'Category C']
-    values = [20, 14, 23]
+    csv_file_path = "static/data/full_data_deaths.csv"
+    countries_of_interest = ['India', 'Pakistan', 'Australia', 'New Zealand']
 
-    # plt.figure(figsize=(8, 6))
-    # plt.bar(categories, values)
-    # plt.title('Bar Chart')
-    # plt.xlabel('Categories')
-    # plt.ylabel('Values')
-    
-    # buffer = io.BytesIO()
-    # plt.savefig(buffer, format='png')
-    # buffer.seek(0)
-    # plot_data = urllib.parse.quote(base64.b64encode(buffer.read()).decode())
+    total_deaths_by_country_and_month, total_cases_by_country_and_month , available_years = process_data(csv_file_path, countries_of_interest)
 
-    countryData=[
-        {
-            'countryName' : 'India',
-            'total_deaths': 1000000,
-            'total_vaccinated': 20000,
-            'total_hospitalizations':10000,
-        },
-        {
-            'countryName' : 'New Zealand',
-            'total_deaths': 2000000,
-            'total_vaccinated': 30000,
-            'total_hospitalizations':20000,
-        },
-        {
-            'countryName' : 'Pakistan',
-            'total_deaths': 3000000,
-            'total_vaccinated': 40000,
-            'total_hospitalizations':30000,
-        },
-        {
-            'countryName' : 'America',
-            'total_deaths': 4000000,
-            'total_vaccinated': 50000,
-            'total_hospitalizations':50000,
-        }
-    ]
+    countryData = []
 
-
-    for country in countryData:
-        # Create a new plot for each country
+    for country in countries_of_interest:
+        # Plot for total deaths
         plt.figure(figsize=(8, 6))
-        plt.bar(categories, values)
-        plt.title(f'Bar Chart - {country["countryName"]}')
-        plt.xlabel('Categories')
-        plt.ylabel('Values')
+        plt.bar(total_deaths_by_country_and_month[country].keys(), total_deaths_by_country_and_month[country].values())
+        plt.title(f'Monthly Deaths - {country}')
+        plt.xlabel('Month')
+        plt.ylabel('Total Deaths')
+        plt.xticks(rotation=45, ha='right')
 
         buffer = io.BytesIO()
         plt.savefig(buffer, format='png')
         buffer.seek(0)
-        country['plot_data'] = urllib.parse.quote(base64.b64encode(buffer.read()).decode())
+        plot_data_deaths = urllib.parse.quote(base64.b64encode(buffer.read()).decode())
 
+        # Plot for total cases
+        plt.figure(figsize=(8, 6))
+        plt.bar(total_cases_by_country_and_month[country].keys(), total_cases_by_country_and_month[country].values())
+        plt.title(f'Monthly Cases - {country}')
+        plt.xlabel('Month')
+        plt.ylabel('Total Cases')
+        plt.xticks(rotation=45, ha='right')
+
+        buffer = io.BytesIO()
+        plt.savefig(buffer, format='png')
+        buffer.seek(0)
+        plot_data_cases = urllib.parse.quote(base64.b64encode(buffer.read()).decode())
+
+        countryData.append({
+            'countryName': country,
+            'total_deaths': 0,  
+            'total_vaccinated': 0,  
+            'total_hospitalizations': 0,  
+            'plot_data_deaths': plot_data_deaths,
+            'plot_data_cases': plot_data_cases,
+        })
 
     context = {
-        'countryData':countryData
+        'countryData':countryData,
+        'available_years':available_years
     }
     return render(request,'home/dashboard.html',context)
