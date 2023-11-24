2c971250264345f2582fef72dc7417bf359823ef
Yash <sadhwaniyash6@gmail.com>
2023-11-25 01:31:06 +0530

    Added year wise data changes further

--- covid_analytics/home/test/test.py
+++ covid_analytics/home/test/test.py
@@ -2,11 +2,11 @@ import csv
 from datetime import datetime
 
 
-def process_data(csv_file_path,countries_of_interest):
-    total_deaths_by_country_and_month = {}
-    total_cases_by_country_and_month = {}
+def process_data(csv_file_path, countries):
+    total_deaths_by_country_and_year = {}
+    total_cases_by_country_and_year = {}
     available_years = set()
-    
+
     with open(csv_file_path, 'r') as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
@@ -15,50 +15,31 @@ def process_data(csv_file_path,countries_of_interest):
             total_deaths = float(row['total_deaths']) if row['total_deaths'] else 0.0
             total_cases = float(row['total_cases']) if row['total_cases'] else 0.0
 
-            if country in countries_of_interest:
-                # Parse the date to get the month
+            if country in countries:
+                # Parse the date to get the month and year
                 date_obj = datetime.strptime(date_str, '%Y-%m-%d')
                 year_key = date_obj.strftime('%Y')
                 month_key = date_obj.strftime('%Y-%m')
 
                 # Initialize the dictionary if it doesn't exist
-                if country not in total_deaths_by_country_and_month:
-                    total_deaths_by_country_and_month[country] = {}
-                if country not in total_cases_by_country_and_month:
-                    total_cases_by_country_and_month[country] = {}
+                if country not in total_deaths_by_country_and_year:
+                    total_deaths_by_country_and_year[country] = {}
+                if country not in total_cases_by_country_and_year:
+                    total_cases_by_country_and_year[country] = {}
 
-                # Update the total deaths for the specific month
-                total_deaths_by_country_and_month[country][month_key] = \
-                    total_deaths_by_country_and_month[country].get(month_key, 0) + total_deaths
-                
-                total_cases_by_country_and_month[country][month_key] = \
-                    total_cases_by_country_and_month[country].get(month_key,0) + total_cases
-                
+                # Initialize the year dictionary if it doesn't exist
+                if year_key not in total_deaths_by_country_and_year[country]:
+                    total_deaths_by_country_and_year[country][year_key] = {}
+                if year_key not in total_cases_by_country_and_year[country]:
+                    total_cases_by_country_and_year[country][year_key] = {}
+
+                # Update the total deaths for the specific month and year
+                total_deaths_by_country_and_year[country][year_key][month_key] = \
+                    total_deaths_by_country_and_year[country][year_key].get(month_key, 0) + total_deaths
+
+                total_cases_by_country_and_year[country][year_key][month_key] = \
+                    total_cases_by_country_and_year[country][year_key].get(month_key, 0) + total_cases
+
                 available_years.add(year_key)
 
-    return total_cases_by_country_and_month , total_deaths_by_country_and_month ,list(available_years)
-# Print the results
-# for country, monthly_data in total_deaths_by_country_and_month.items():
-#     print(f"{country}:")
-#     for month, total_deaths in monthly_data.items():
-#         print(f"  {month}: {total_deaths}")
-
-
-# for country, monthly_data in total_cases_by_country_and_month.items():
-#     print(f"{country}:")
-#     for month, total_cases in monthly_data.items():
-#         print(f"  {month}: {total_cases}")
-
-
-
-
-# with open("../../static/data/covid-hospitalizations.csv") as csv_file2:
-#     reader = csv.DictReader(csv_file2)
-#     header = reader.fieldnames
-#     print(header)
-
-
-# with open("../../static/data/excess_mortality.csv") as csv_file3:
-#     reader = csv.DictReader(csv_file3)
-#     header = reader.fieldnames
-#     print(header)
+    return total_cases_by_country_and_year, total_deaths_by_country_and_year, list(available_years)
